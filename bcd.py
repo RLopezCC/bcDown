8d96a6ad573f737cb4a8d8e733bb341dbfe3c360
Rodrigo Lopez <roko120@hotmail.com>
2013-08-27 19:32:14 -0300

    Obsolete imports removed

--- bcd.py
+++ bcd.py
@@ -1,56 +1,53 @@
-import urllib2
 import urllib
-import sys
-import re
-from urllib2 import HTTPError, URLError
+
 
 class record:
-	bandname = ""
-	recordname = ""
-	bandcampLink = ""
-	artwork = ""
-	songsnum = 0
-	songnames = []
-	songlinks = []
-	
-	def getRecordInfo(self):
-		songs_sum = 0
-		trackinfofound=False
-		f = getURLFile(self.bandcampLink)
-		for line in f.readlines():
-			if "<title>" in line:
-				bandname = line.split('|')[1]
-				bandname = bandname.split('<')[0]
-				
-				recordname = line.split('|')[0]
-				recordname = recordname.split('>')[1]
-			if "artFullsizeUrl" in line:
-				self.artwork = line.split("\"")[1]
-			if "info_link" in line:
-				songs_sum+=1
-			if "trackinfo" in line and trackinfofound==False:
-				trackinfo = line
-				trackinfofound=True
-				trackinfo = trackinfo.split("{")
-				for line in trackinfo:
-					if "mp3-128" in line:
-						link = line.split("\"")[3]
-						self.songlinks.append(link)
-						
-						name = line.split("title")[1]
-						name = name.split("\"")[2]
-						self.songnames.append(name)
+    bandname = ""
+    recordname = ""
+    bandcampLink = ""
+    artwork = ""
+    songsnum = 0
+    songnames = []
+    songlinks = []
 
-		f.close()
-		self.bandname = bandname
-		self.recordname = recordname
-		self.songsnum = songs_sum
-		
-	def __init__(self, bcurl):
-		self.bandcampLink = bcurl
-		self.getRecordInfo()
-		
+    def getRecordInfo(self):
+        songs_sum = 0
+        trackinfofound = False
+        f = get_url_file(self.bandcampLink)
+        for line in f.readlines():
+            if "<title>" in line:
+                bandname = line.split("|")[1]
+                bandname = bandname.split("<")[0]
+                self.bandname = bandname
 
-def getURLFile(url):
-		f= urllib2.urlopen(url)
-		return f
+                recordname = line.split("|")[0]
+                recordname = recordname.split(">")[1]
+                self.recordname = recordname
+            if "artFullsizeUrl" in line:
+                self.artwork = line.split("\"")[1]
+
+            if "info_link" in line:
+                songs_sum += 1
+            if "trackinfo" in line and trackinfofound is False:
+                trackinfo = line
+                trackinfofound = True
+                trackinfo = trackinfo.split("{")
+                for line in trackinfo:
+                    if "mp3-128" in line:
+                        link = line.split("\"")[3]
+                        self.songlinks.append(link)
+
+                        name = line.split("title")[1]
+                        name = name.split("\"")[2]
+                        self.songnames.append(name)
+            self.songsnum = songs_sum
+        f.close()
+
+    def __init__(self, bcurl):
+        self.bandcampLink = bcurl
+        self.getRecordInfo()
+
+
+def get_url_file(url):
+    f = urllib.urlopen(url)
+    return f
